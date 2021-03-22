66eebea10be0602b98d3b87209605f0fd2e565e2
SwarajShingote <swarajanilshingote@gmail.com>
2021-03-23 01:32:15 +0530

    Updation

--- houseofindya_scraper/items.py
+++ houseofindya_scraper/items.py
@@ -4,9 +4,8 @@ import scrapy
 class item_hoi(scrapy.Item):
 
     """ Defining field items for 
-         desc, price and url"""
+         description, price and url"""
 
     desc = scrapy.Field()
     price = scrapy.Field()
     url = scrapy.Field()
-    
